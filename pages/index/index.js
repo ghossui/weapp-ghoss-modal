7d9f97afb539d59b4792fd5a1b1849c1b4bd0f56
孙建雷 <leon@sjlei.com>
2018-11-03 12:36:50 +0800

    新增控制动画效果，内置三种动画

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,23 @@
 const app = getApp();
-const GhossModal = require("../../lib/ghoss-modal/gmodal.js");
+const GhossModal = require("../../lib/ghoss-modal/gmodal.util.js");
+// import { GhossModal } from "../../lib/ghoss-modal/gmodal.util.js";
 var _this;
 var title = "弹窗标题";
 var content = "弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内。";
 Page({
 
-  data: {},
+  data: {
+    theme: {
+      range: ["normal", "wx-android", "wx-ios"],
+      index: 1,
+      value: "wx-android",
+    },
+    animation: {
+      range: ["none", "zoom-in-out", "fading-in-out", "fading-zoom-in-out"],
+      index: 1,
+      value: "fading-zoom-in-out",
+    }
+  },
 
   onLoad() {
     _this = this;
@@ -15,6 +27,45 @@ Page({
 
     // _this.onTapShowAlert();
 
+  },
+
+  onSubmitShowModal(event) {
+    const value = event.detail.value;
+
+    value.confirm = (res) => {
+      wx.showToast({ title: "confirm", icon: "none" })
+    };
+
+    value.cancel = (res) => {
+      wx.showToast({ title: "cancel", icon: "none" })
+    };
+
+    value.complete = (res) => {
+      console.log("complete：", res)
+    };
+
+    // value.animation = "none";
+    // value.animation = "zoom-in-out";
+    // value.animation = "fading-in-out";
+
+    console.log("value:", value)
+
+    _this.gmodal.show("gmodal.showModal", value);
+  },
+
+  onChangeTheme(event) {
+    let idx = parseInt(event.detail.value);
+    _this.setData({
+      "theme.index": idx,
+      "theme.value": _this.data.theme.range[idx]
+    });
+  },
+  onChangeAnimation(event) {
+    let idx = parseInt(event.detail.value);
+    _this.setData({
+      "animation.index": idx,
+      "animation.value": _this.data.animation.range[idx]
+    });
   },
 
   onTapShowNormal() {
